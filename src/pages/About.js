5e8a08b8f23da3bed5a781f6866778837b264b27
Anubhav Sigdel <64096825+asigdel29@users.noreply.github.com>
2023-05-19 15:23:53 -0400

    reverted modal styling without animation

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,13 @@
-import React, { useState, createRef } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Layout from '../Components/Layout';
 import '../Styles/styles.css';
-import { CSSTransition } from 'react-transition-group';
-//import { createPortal } from 'react-dom';
 
-const Modal = ({ isOpen, onClose }) => {
+const Modal = ({ isOpen, onClose, isAnimatingClose }) => {
     return (
-        <CSSTransition in={isOpen} timeout={1000} classNames="unfold">
-            <div className="modal-content">
+        <div className={isOpen ? "modal open" : isAnimatingClose ? "modal closing" : "modal"}>
+            <div className={isOpen ? "modal-content open" : isAnimatingClose ? "modal-content closing" : "modal-content"}>
+                <span className="close" onClick={onClose}>&times;</span>
                 <p>
                     As an international student from Nepal pursuing a double major in computer science and cognitive science, I have always been fascinated by the intersection of technology and the human mind. This curiosity led me to pursue a career in software engineering, where I can apply my technical and cognitive skills to solve real-world problems.
                 </p>
@@ -32,135 +31,138 @@ const Modal = ({ isOpen, onClose }) => {
                     <li>Machine Learning: TensorFlow, Keras, Natural Language Processing, Spark</li>
                 </ul>
             </div>
-        </CSSTransition>
+        </div>
     );
 };
 
 const About = () => {
     const navigate = useNavigate();
     const [isBioOpen, setIsBioOpen] = useState(false);
-    const nodeRef = createRef(null);
+    const [isAnimatingClose, setIsAnimatingClose] = useState(false);
 
     const closeModal = () => {
-        setIsBioOpen(false);
+        setIsAnimatingClose(true);
+        setTimeout(() => {
+            setIsBioOpen(false);
+            setIsAnimatingClose(false);
+        }, 1500);
     };
 
-    const getCommands = (setIsBioOpen) => {
-        return {
+    const commands = {
+        ls: {
+            description: 'Show everything on the page',
+            fn: function () {
+                return 'Resume\nLinkedin\nGithub\nEmail\nBio';
+            }
+        },
 
-            ls: {
-                description: 'Show everything on the page',
-                fn: function () {
-                    return 'Resume\nLinkedin\nGithub\nEmail\nBio';
-                }
-            },
+        about: {
+            description: 'You are here',
+            fn: function () {
+                return 'You are already at the about page';
+            }
+        },
 
-            about: {
-                description: 'You are here',
-                fn: function () {
-                    return 'You are already at the about page';
-                }
-            },
+        projects: {
+            description: 'Go to Projects page',
+            fn: function () {
+                setTimeout(function () {
+                    navigate('/projects');
+                }, 1200);
+                return 'Redirecting to the projects Page ...';
+            }
+        },
 
-            projects: {
-                description: 'Go to Projects page',
-                fn: function () {
-                    setTimeout(function () {
-                        navigate('/projects');
-                    }, 1200);
-                    return 'Redirecting to the projects Page ...';
-                }
-            },
+        research: {
+            description: 'Go to Research page',
+            fn: function () {
+                setTimeout(function () {
+                    navigate('/research');
+                }, 1200);
+                return 'Redirecting to the Research Page ...';
+            }
+        },
 
-            research: {
-                description: 'Go to Research page',
-                fn: function () {
-                    setTimeout(function () {
-                        navigate('/research');
-                    }, 1200);
-                    return 'Redirecting to the Research Page ...';
-                }
-            },
 
-            misc: {
-                description: 'Go to Misc page',
-                fn: function () {
-                    setTimeout(function () {
-                        navigate('/misc');
-                    }, 1200);
-                    return 'Redirecting to the Miscellaneous Page ...';
-                }
-            },
+        misc: {
+            description: 'Go to Misc page',
+            fn: function () {
+                setTimeout(function () {
+                    navigate('/misc');
+                }, 1200);
+                return 'Redirecting to the Miscellaneous Page ...';
+            }
+        },
 
-            cd: {
-                description: 'Go to Main page',
-                fn: function () {
-                    setTimeout(function () {
-                        navigate('/');
-                    }, 1200);
-                    return 'Redirecting to the Main Page ...';
-                }
-            },
+        cd: {
+            description: 'Go to Main page',
+            fn: function () {
+                setTimeout(function () {
+                    navigate('/');
+                }, 1200);
+                return 'Redirecting to the Main Page ...';
+            }
+        },
 
-            Resume: {
-                description: 'Go to Resume page',
-                fn: function () {
-                    setTimeout(function () {
-                        window.open('https://cs.oswego.edu/~asigdel/Resume.pdf', '_blank');
-                    }, 1200);
-                    return 'Redirecting to the resume page...';
-                }
-            },
+        Resume: {
+            description: 'Go to Resume page',
+            fn: function () {
+                setTimeout(function () {
+                    window.open('https://cs.oswego.edu/~asigdel/Resume.pdf', '_blank');
+                }, 1200);
+                return 'Redirecting to the resume page...';
+            }
+        },
 
-            Linkedin: {
-                description: 'Go to Linkedin profile',
-                fn: function () {
-                    setTimeout(function () {
-                        window.open('https://www.linkedin.com/in/asigdel/', '_blank');
-                    }, 1200);
-                    return 'Redirecting to the Linkedin Profile ...';
-                }
-            },
 
-            Github: {
-                description: 'Go to Github profile',
-                fn: function () {
-                    setTimeout(function () {
-                        window.open('https://www.github.com/asigdel29/', '_blank');
-                    }, 1200);
-                    return 'Redirecting to the Github Profile ...';
-                }
-            },
+        Linkedin:{
+            description: 'Go to Linkedin profile',
+            fn: function () {
+                setTimeout(function () {
+                    window.open('https://www.linkedin.com/in/asigdel/', '_blank');
+                }, 1200);
+                return 'Redirecting to the Linkedin Profile ...';
+            }
+        },
 
-            Email: {
-                description: 'Shoot me an email',
-                fn: function () {
-                    setTimeout(function () {
-                        window.open('mailto:sigdelanubhav@gmail.com', '_blank');
-                    }, 1200);
-                    return 'I tend to respond quick';
-                }
-            },
-            Bio: {
-                description: 'A little about me',
-                fn: function () {
+
+        Github:{
+            description: 'Go to Github profile',
+            fn: function () {
+                setTimeout(function () {
+                    window.open('https://www.github.com/asigdel29/', '_blank');
+                }, 1200);
+                return 'Redirecting to the Github Profile ...';
+            }
+        },
+
+
+        Email: {
+            description: 'Shoot me an email',
+            fn: function () {
+                setTimeout(function () {
+                    window.open('mailto:sigdelanubhav@gmail.com', '_blank');
+                }, 1200);
+                return 'I tend to respond quick';
+            }
+        },
+
+        Bio: {
+            description: 'A little about me',
+            fn: function () {
+                setTimeout(function () {
                     setIsBioOpen(true);
-                    console.log('Opening Bio...');
-                    return 'Here it comes...';
-                }
-            },
-        };
+                }, 1200);
+                return 'Here it comes...';
+            }
+        }
     };
-
-    const commands = getCommands(setIsBioOpen);
 
     return (
-        <Layout commands={commands}
-                welcomeMessage="Yoou've made it to the About page. Enter command 'ls' to find out more. Use 'cd' to go back to main">
-            <CSSTransition nodeRef={nodeRef} in={isBioOpen} timeout={1000} classNames="unfold" unmountOnExit>
-                <Modal ref={nodeRef} isOpen={isBioOpen} onClose={closeModal} />
-            </CSSTransition>
-            {/* other components in Layout */}
+        <Layout commands={commands} welcomeMessage="Yoou've made it to the About page. Enter command 'ls' to find out more. Use 'cd' to go back to main">
+            {(isBioOpen || isAnimatingClose) && (
+                <Modal isOpen={isBioOpen} onClose={closeModal} isAnimatingClose={isAnimatingClose} />
+            )}
         </Layout>
     );
 }
