914fe8a0c782441f65e329c2ca8a4a1d63d5c27d
asigdel29 <sigdelanubhav@gmail.com>
2023-10-09 19:53:46 -0400

    Edited content for Robot Radar

--- src/Components/Project Page/PageContent/RobotRadarContent.jsx
+++ src/Components/Project Page/PageContent/RobotRadarContent.jsx
@@ -1,8 +1,38 @@
 const RobotRadarContent = `
-description:'Utilizing agile software development methodology, I have created a highly efficient and accurate solution for your Pi robots.\n' +
-'I programmed the radar in Python, enabling it to collect LIDAR sensor readings with ease. Through advanced trigonometric computations, I have achieved a remarkable 95% accuracy in calculating target coordinates. This ensures that your robots can navigate their environment with unparalleled precision and safety.\n' +
-'\n' +
-'With my radar application, you can trust that your Pi robots will be equipped with the most advanced technology available. Plus, I am always available to provide support and updates as needed.',
-repoLink: https://github.com/asigdel29/RobotRadar,
-    `;
+<p>
+In a collaborative effort with the Cyber Physical Systems Laboratory at SUNY Oswego, I embarked on a journey to enhance the capabilities of Raspberry Pi-powered robots. These surrogate platforms, designed for autonomous systems, already employed a plethora of sensors like line detectors, distance sensors, and inertial sensors. Paired with ultrasonic translateralization beacons, they boasted an impressive location awareness. Yet, there was room for improvement.
+</p>
+
+<p>
+A significant challenge arose when multiple robots detected an obstacle; they couldn't discern if it was the same one. This differentiation was crucial for autonomous team navigation. Hence, the objective was clear: design and implement an obstacle "radar" application that not only detects obstacles but can also differentiate between them when detected by multiple robots.
+</p>
+
+<p>
+Utilizing agile software development methodologies, I created a robust solution. The radar was programmed in Python, efficiently collecting LIDAR sensor readings. Through intricate trigonometric computations, it achieved a remarkable 95% accuracy in calculating target coordinates. This ensured the robots could identify and navigate around obstacles with unparalleled precision.
+</p>
+
+<p>
+Key features of the application include:
+<ul>
+<li>Ability to control the GoPiGo2 robots and read sensor data</li>
+<li>Display obstacles on a user-friendly interface</li>
+<li>Heuristic approach to differentiate between obstacles</li>
+<li>Consideration of robot and obstacle movement, both absolute and relative to the robot's position</li>
+</ul>
+</p>
+
+<p>
+This project was realized in cooperation with Bastian Tenbergen. It required not just technical prowess but also adept stakeholder communication, understanding constraints, and working towards the project goals effectively.
+</p>
+
+<p>
+For a closer look into the technicalities and codebase, you can visit the repository: <a href="https://github.com/asigdel29/RobotRadar" style="color: #00FF00; text-decoration: underline;">GitHub</a>.
+</p>
+
+<p>
+<img src="./path_to_first_image.jpg" alt="Robot Radar Image 1" />
+<img src="./path_to_second_image.jpg" alt="Robot Radar Image 2" />
+</p>
+`;
+
 export default RobotRadarContent;
