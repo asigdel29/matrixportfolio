630f6e49efed54a2468d29489663c1c8a72fad35
asigdel29 <sigdelanubhav@gmail.com>
2023-10-09 19:36:14 -0400

    Edit CPRContent.jsx

--- src/Components/Project Page/PageContent/CPRContent.jsx
+++ src/Components/Project Page/PageContent/CPRContent.jsx
@@ -1,17 +1,34 @@
 const CPRContent = `
-<p>The CPR Tool is the product of a software design project that functions to faclitate the peer review process in an academic setting. 
-A calibrated peer review refers to the process in which an instructor creates and assignment, distributes the assignment to be completed 
-by students, reviews their solutions, disperses the solution to other students for peer review, and then quality checks each review before assigning a a final grade.</p>
+<p>The Calibrated Peer Review (CPR) Tool is the result of an intensive software design project tailored to streamline the peer review process in academic settings. A calibrated peer review is a method wherein an instructor devises an assignment, assigns it to students, evaluates their solutions, disseminates the solution to peers for review, and conducts quality assurance on each review before determining the final grade.</p>
 
-<p>The software design team utilized an agile development methodology to create an anonymous peer review platform, which reduced biases and increased the reliability of reviews.</p> 
+<p>Our software design team harnessed an agile development methodology, culminating in an anonymous peer review platform. This innovation curtailed biases and heightened the dependability of the reviews.</p>
 
-<p>The development of the CPR Tool was carried out by a SUNY Oswego software design team with oversight from IBM, who acted as stakeholders throughout the project.
-The Oswego team worked closely with IBM to incorporate their feedback and suggestions into the design of the application, ensuring it met the needs of both students and instructors alike.
-In addition, the development team utilized IBM technology throughout the project to create a powerful and scalable platform.</p> 
+<p>The development was executed under the umbrella of a SUNY Oswego software design team, coupled with the continuous guidance from IBM, who played the role of pivotal stakeholders throughout the project's journey. The harmonious collaboration with IBM enabled the assimilation of valuable feedback and insights into the design, ensuring the application aptly catered to the requisites of both educators and students. The project also saw the extensive employment of IBM's state-of-the-art technology, resulting in a robust and scalable solution.</p>
 
-<p>Here is the repository for the code, <a href="https://github.com/tenbergen/CSC480-22S" style="color: #00FF00; text-decoration: underline;">GitHub</a>.</p>
-<p>Here is the live link, <a href="https://moxie.cs.oswego.edu:13125/about" style="color: #00FF00; text-decoration: underline;">CPR Tool</a>.</p>
-<P>Here is the Quest Research Symposium <a href="https://docs.google.com/presentation/d/1kNB0mHmsUm6jLow_vsxPghY7nEEcr8JHPYeguZxLImw/edit?usp=sharing" style="color: #00FF00; text-decoration: underline;">Presentation</a>.</p>
+<p>Delving deeper into the specifics of the project:</p>
+<ul>
+    <li><b>Role:</b> Full Stack Developer</li>
+    <li><b>Collaboration:</b> Joint effort with IBM stakeholders</li>
+    <li><b>Challenge:</b> Augmentation of backend functionalities of an academic tool to magnify its usability and adoption.</li>
+    <li><b>Technologies & Tools:</b></li>
+    <ul>
+        <li>Languages & Frameworks: Java, JavaScript, React</li>
+        <li>Microservice Architecture: Independent services using Open Liberty and Nginx</li>
+        <li>Deployment: Docker for consistent containerized applications</li>
+        <li>Database: MongoDB for data management and organization</li>
+        <li>Testing: Junit, Selenium for testing, and Postman for API checks</li>
+        <li>Project Management & Collaboration: Agile methodologies and Trello for task orchestration</li>
+        <li>Documentation: Comprehensive technical requirements and specs</li>
+    </ul>
+    <li><b>Outcome:</b> Successful launch of the CPR Tool, receiving widespread acclaim and adoption by the academic community.</li>
+</ul>
+
+<p>For an in-depth dive into our work:</p>
+<ul>
+    <li><a href="https://github.com/tenbergen/CSC480-22S" style="color: #00FF00; text-decoration: underline;">GitHub Repository</a></li>
+    <li><a href="https://moxie.cs.oswego.edu:13125/about" style="color: #00FF00; text-decoration: underline;">Live CPR Tool</a></li>
+    <li><a href="https://docs.google.com/presentation/d/1kNB0mHmsUm6jLow_vsxPghY7nEEcr8JHPYeguZxLImw/edit?usp=sharing" style="color: #00FF00; text-decoration: underline;">Quest Research Symposium Presentation</a></li>
+</ul>
 `;
 
 export default CPRContent;
