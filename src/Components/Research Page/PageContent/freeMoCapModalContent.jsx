e177109e7b5202bae710713e9c301ffe8773bf65
asigdel29 <asigdel@oswego.edu>
2023-10-09 22:09:54 -0400

    modified content in modal

--- src/Components/Research Page/PageContent/freeMoCapModalContent.jsx
+++ src/Components/Research Page/PageContent/freeMoCapModalContent.jsx
@@ -1,7 +1,12 @@
-const freeMoCapModalContent =`
-<p>As a researcher for the Cognitive Dynamics Lab at SUNY Oswego, I developed a research-grade markerless motion capture program...</p>
-<p>For further information about this project, you can check it out on <a href="https://github.com/asigdel29/freemocap" style="color: #00FF00; text-decoration: underline;">GitHub</a>.</p>
-<p>You can also check out my talk for QUEST Research Symposium <a href="https://docs.google.com/presentation/d/1wzvsPKkncVc5REO9GTR4l64NAx8Hnsi2qmCLuc-iLeo/edit#slide=id.p" style="color: #00FF00; text-decoration: underline;">here</a>.</p></p>
+const freeMoCapModalContent = `
+<p>As a research assistant for the Cognitive Dynamics Lab at SUNY Oswego, I played a key role in developing a high-precision markerless motion capture program...</p>
+<p>I worked at the Cognitive Dynamics Lab, SUNY Oswego, from August 2021 to May 2023.</p>
+<ul>
+  <li>My primary responsibility was to enhance the precision of motion capture for research purposes. To achieve this, I engineered a markerless motion capture program using OpenPose and DeepLabCu. This program achieved an impressive calibration accuracy of 99.3% when using Charuco Boards. This precision ensured near-perfect capture of motion data for our research studies.</li>
+  <li>In addition to developing the motion capture program, I played a critical role in data collection. Participants in our research completed a series of Fitts Chopsticks Tasks in three different settings. These tasks involved moving objects back and forth between specific areas to measure tradeoffs between speed and accuracy. Participants performed the tasks with their fingers and with chopsticks, and then repeated both tasks while blindfolded.</li>
+</ul>
+<p>If you'd like to explore this project further, you can find it on <a href="https://github.com/asigdel29/freemocap" style="color: #00FF00; text-decoration: underline;">GitHub</a>.</p>
+<p>You can also view my presentation for the QUEST Research Symposium <a href="https://docs.google.com/presentation/d/1wzvsPKkncVc5REO9GTR4l64NAx8Hnsi2qmCLuc-iLeo/edit#slide=id.p" style="color: #00FF00; text-decoration: underline;">here</a>.</p>
 `;
 
 export default freeMoCapModalContent;
