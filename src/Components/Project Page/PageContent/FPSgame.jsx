8f1bbef29ca91e45e0939b089014a8ce04a54972
asigdel29 <sigdelanubhav@gmail.com>
2023-10-09 20:27:57 -0400

    Added FPS game content

---
+++ src/Components/Project Page/PageContent/FPSgame.jsx
@@ -0,0 +1,30 @@
+const FPSGameContent = `
+<p>
+<strong>Role:</strong> Game Developer Intern
+</p>
+<p>
+<strong>Collaboration:</strong> Collimation (VR Startup)
+</p>
+<p>
+<strong>Objective:</strong> Develop a multi-dimensional gaming experience allowing users to immerse in virtual concerts and engage with their favorite artists through Virtual Reality (VR) and Augmented Reality (AR).
+</p>
+<p>
+<strong>Technologies & Tools:</strong>
+<ul>
+  <li>Languages: C++ for prototype development.</li>
+  <li>Game Engine: Unreal Engine for game design and music-driven locomotion.</li>
+  <li>Cloud Computing: Utilized AWS EC2 virtual instances to foster real-time team collaboration.</li>
+  <li>VR/AR: Incorporated immersive experiences enabling users to attend live concerts virtually or bring AR concerts to their mobile devices.</li>
+</ul>
+</p>
+<p>
+<strong>Key Responsibilities & Achievements:</strong>
+<ul>
+  <li>Spearheaded the development of game prototypes aligning with design team's specifications.</li>
+  <li>Successfully introduced a music-driven locomotion mechanism, elevating the third-person shooter gameplay experience.</li>
+  <li>Closely collaborated with the Creative and Immersive teams, playing a pivotal role in assimilating design features into games, ensuring readiness before market launch.</li>
+</ul>
+</p>
+`;
+
+export default FPSGameContent;
