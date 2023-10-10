7cef70d7e7b46459c1deeb1204235f48ce80dd85
asigdel29 <asigdel@oswego.edu>
2023-10-09 22:03:20 -0400

    added content for CPS modal

--- src/Components/Research Page/PageContent/cpsModalContent.jsx
+++ src/Components/Research Page/PageContent/cpsModalContent.jsx
@@ -1,5 +1,12 @@
-const cpsModalContent =`
+const cpsModalContent = `
 <p>As a researcher at the Cyber Physical Systems Lab SUNY Oswego, I developed a Collaborative Adaptive Cruise Control (CACC) system...</p>
+<p>The simulation is divided into three phases to simulate Collaborative Adaptive Cruise Control behavior:</p>
+<ol>
+  <li>Phase 1: Initiate</li>
+  <li>Phase 2: Sensing and Control</li>
+  <li>Phase 3: Negotiation</li>
+</ol>
+<p>I also attached a Logitech steering wheel to simulate "autonomous" driving by mimicking cars in the simulation.</p>
 <p>For further information about this project, you can check it out on <a href="https://github.com/tenbergen/Urban-CPS" style="color: #00FF00; text-decoration: underline;">GitHub</a>.</p>
 `;
 
