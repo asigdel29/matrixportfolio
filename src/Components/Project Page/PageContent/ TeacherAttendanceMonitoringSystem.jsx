205a73ca448b52c846deca7b97e1bd10ec43a230
asigdel29 <sigdelanubhav@gmail.com>
2023-10-09 20:26:23 -0400

    Added TMAS content

---
+++ src/Components/Project Page/PageContent/ TeacherAttendanceMonitoringSystem.jsx
@@ -0,0 +1,12 @@
+const TeacherAttendanceMonitoringSystem = () => (
+    <div>
+        <h2>Teacher Attendance Monitoring System</h2>
+        <h3>Role: Software Engineer Intern</h3>
+        <p><strong>Collaboration:</strong> Collaborated with IPE Global on a World Bank-funded initiative.</p>
+        <p><strong>Design & Development:</strong> Spearheaded the creation of a comprehensive Attendance Monitoring System.</p>
+        <p><strong>Technologies Used:</strong> Integrated AWS facial recognition, complemented by geo-fencing, and multi-factor authentication, into both a mobile and web application.</p>
+        <p><strong>Software QA:</strong> Conducted thorough requirement analysis, designed test cases, and performed both manual and automated testing. Engaged in system, integration, and user acceptance testing to ensure system robustness. Adopted continuous integration tools for real-time bug tracking and reporting.</p>
+    </div>
+);
+
+export default TeacherAttendanceMonitoringSystem;
