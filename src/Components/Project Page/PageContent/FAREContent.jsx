37161e2f4f51cbfa756ff720cbf0f30d7ed8853b
asigdel29 <sigdelanubhav@gmail.com>
2023-10-09 20:30:30 -0400

    Added FARE app content and edited other content.jsx files

---
+++ src/Components/Project Page/PageContent/FAREContent.jsx
@@ -0,0 +1,24 @@
+const FAREContent = `
+<p>
+<strong>Role:</strong> Full Stack Developer
+</p>
+<p>
+<strong>Technologies & Tools:</strong>
+<ul>
+  <li>Frontend/Backend Development: Java, JavaScript(React)</li>
+  <li>Database: MongoDB</li>
+  <li>Cloud & Hosting: Amazon Web Services</li>
+  <li>Others: Version Control: Git, Containerization: Docker</li>
+</ul>
+</p>
+<p>
+<strong>Key Responsibilities & Achievements:</strong>
+<ul>
+  <li>Ideated, conceptualized, and brought to fruition the FARE platform as the sole developer and founder.</li>
+  <li>Successfully eliminated manual invoicing processes by integrating smart contracts, ensuring the automation of agreement and payment stages.</li>
+  <li>Spearheaded the implementation of a robust escrow system, bolstering security and fostering trust among platform users.</li>
+</ul>
+</p>
+`;
+
+export default FAREContent;
